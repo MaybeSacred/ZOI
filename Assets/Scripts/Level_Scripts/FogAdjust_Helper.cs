7429949b8c5b5eb134f56a440a4f808c4cab7cbf
ddias3 <danieldias15@gmail.com>
2014-04-01 23:58:32 -0400

    Fog is now smooth throughout the entire campaign.
    Turns off for the last level.

---
+++ Assets/Scripts/Level_Scripts/FogAdjust_Helper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogAdjust_Helper : MonoBehaviour
+{
+	public float fogDensity;
+	public FogAdjust_Helper previousTrigger;
+	public FogAdjust_Helper nextTrigger;
+
+	private FogAdjust fogAdjust;
+
+	void Start()
+	{
+		fogAdjust = this.transform.parent.gameObject.GetComponent<FogAdjust>();
+	}
+
+	void OnTriggerEnter(Collider tank)
+	{
+		if (tank.tag == "Player")
+		{
+			fogAdjust.OnHelperTrigger(this);
+			gameObject.SetActive(false);
+		}
+	}
+}
