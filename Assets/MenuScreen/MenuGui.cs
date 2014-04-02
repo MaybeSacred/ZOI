058966a2b361eddb93469c3f873cb3b1cbe80a57
Erica Pramer <erica.pramer@gatech.edu>
2014-04-01 23:44:34 -0400

    finished the options section of the menuGUI

--- Assets/MenuScreen/MenuGui.cs
+++ Assets/MenuScreen/MenuGui.cs
@@ -5,12 +5,23 @@ public class MenuGui : MonoBehaviour {
 	
 	public GUIStyle mainStyle; //background = menuTexture
 	public GUIStyle buttonStyle; //ICE AGE font, black size 50 aligned middle center, normal background = whitebar, active background = whitebarpressed
+	public GUIStyle leftButtonStyle;
+	public GUIStyle rightButtonStyle;
+	public GUIStyle mouseSensitivityButtonStyle; //size 40, upper center
+	public GUIStyle currentSensitivityStyle;
 	public GUIStyle menuTitleStyle; //IceCaps font, white size 65
 	public GUIStyle titleStyle; //IceCaps white size 115, aligned middle center
 
+	private CameraScript theCamera;
+
 	int imagex,imagey,imagewidth,imageheight;
+	bool main = true;
 
 	void Start () {
+
+		//stuff that makes it snow :D
+		theCamera = GetComponent<CameraScript>();
+
 		imagex = Screen.width-490;
 		imagey = 0;
 		imagewidth = 500;
@@ -24,7 +35,10 @@ public class MenuGui : MonoBehaviour {
 
 	void OnGUI()
 	{
-		MenuScreen();
+		if(main==true)
+			MenuScreen();
+		else
+			Options();
 	}
 
 	void MenuScreen()
@@ -34,11 +48,35 @@ public class MenuGui : MonoBehaviour {
 		GUI.Label(new Rect(400,200,100,100), "Zone" + "\n" + "of" + "\n" + "Inaccessibility", titleStyle);
 		if(GUI.Button(new Rect(imagex+100,imagey+250,imagewidth-200,50), "Start Game", buttonStyle))
 		{
-			//do stuff
+			Application.LoadLevel(1);
 		}
 		if(GUI.Button(new Rect(imagex+100,imagey+350,imagewidth-200,50), "Options", buttonStyle))
 		{
-			//do stuff
+			main = false;
 		}
 	}
+
+	void Options()
+	{
+		GUI.BeginGroup(new Rect(Screen.width/2 - imagewidth/2, Screen.height/2 - 300, imagewidth, imageheight));
+		GUI.Box(new Rect(0, 0, imagewidth, imageheight), "", mainStyle);
+		GUI.Label(new Rect(125,95,250,50), "Options", menuTitleStyle);
+		GUI.Label(new Rect(125,210,250,50), "Mouse Sensitivity", mouseSensitivityButtonStyle);
+
+		if(GUI.Button(new Rect(100,imageheight/2-35,50,50), "", leftButtonStyle)) 
+		{
+			theCamera.DecrementMouseSensitivity();
+		}
+		if(GUI.Button(new Rect(350,imageheight/2-35,50,50),"",rightButtonStyle))
+		{
+			theCamera.IncrementMouseSensitivity();
+		}
+		if(GUI.Button(new Rect(155,imageheight/2+125,200,50), "OK", buttonStyle))
+		{
+			main = true;
+		}
+		GUI.TextArea(new Rect(235, imageheight/2-35, 224, 48), theCamera.GetCurrentMouseSensitivity().ToString(), currentSensitivityStyle);
+		GUI.EndGroup();
+	}
+
 }
