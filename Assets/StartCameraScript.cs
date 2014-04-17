3d52640af6a26ed682facd49d742912f67ac6fa8
Jon <jtyson3@gatech.edu>
2014-04-17 00:49:15 -0400

    Working title screen :D

---
+++ Assets/StartCameraScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class StartCameraScript : MonoBehaviour {
+	public float nextPointTime;
+	public float cameraLookOffset;
+	private float pointTimer;
+	private int currentLookAtPoint;
+	public Transform[] pointsToLookAt;
+	public float rotationSpeed;
+	void Start () {
+		Vector3 randVec = Random.onUnitSphere;
+		randVec.y = 0;
+		randVec.Normalize();
+		randVec.y = .1f;
+		transform.position = pointsToLookAt[currentLookAtPoint].position + cameraLookOffset * randVec;
+		transform.LookAt(pointsToLookAt[currentLookAtPoint].position);
+	}
+	
+	void Update () {
+		pointTimer += Time.deltaTime;
+		if(pointTimer > nextPointTime)
+		{
+			pointTimer = 0;
+			if(currentLookAtPoint >= pointsToLookAt.Length)
+			{
+				currentLookAtPoint = 0;
+			}
+			else
+			{
+				currentLookAtPoint++;
+			}
+			Vector3 randVec = Random.onUnitSphere;
+			randVec.y = 0;
+			randVec.Normalize();
+			randVec.y = .1f;
+			transform.position = pointsToLookAt[currentLookAtPoint].position + cameraLookOffset * randVec;
+			transform.LookAt(pointsToLookAt[currentLookAtPoint].position);
+		}
+		transform.localRotation *= Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.up);
+	}
+}
